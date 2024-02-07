b138622f47cbd2b92b3d776417411a6e94aeba48
Marcello Chiaramonte <marcello.chiaramonte@contractors.roche.com>
2024-02-07 13:57:16 +0100

    Initial commit

---
+++ src/main/java/coding/test/trading/Controller.java
@@ -0,0 +1,15 @@
+package coding.test.trading;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/api/hello")
+    private String getHello(){
+        return "hello";
+    }
+
+
+}
---
+++ src/main/java/coding/test/trading/model/Instrument.java
@@ -0,0 +1,16 @@
+package coding.test.trading.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="instruments")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Instrument {
+    
+}
